e02eca1b130fd1d90b6838302778291ef66244e2
wwb <22379099@qq.com>
2023-12-07 12:53:50 +0800

    1.0.5

---
+++ src/WWB.Tools/DateHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WWB.Tools
+{
+    public static class DateHelper
+    {
+        public static long DateTimeToUnixTime(DateTime dateTime)
+        {
+            return (dateTime.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+        }
+
+        public static DateTime UnixTimeToDateTime(long timeStamp)
+        {
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)); // 当地时区
+
+            return startTime.AddTicks(timeStamp * 10000000);
+        }
+    }
+}
