a2c4f62bdf3b30137351ae1c46901401b6b252f0
wwb <22379099@qq.com>
2023-12-08 14:41:58 +0800

    1.0.8

--- src/WWB.Tools/Extensions/SerializerExtensions.cs
+++ src/WWB.Tools/Extensions/SerializerExtensions.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.RegularExpressions;
 
 namespace WWB.Tools.Extensions
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public static class SerializerExtensions
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
@@ -20,7 +20,7 @@ namespace WWB.Tools.Extensions
         }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="val"></param>
         /// <typeparam name="T"></typeparam>
@@ -28,6 +28,22 @@ namespace WWB.Tools.Extensions
         public static T ToObject<T>(this string val)
         {
             return JsonConvert.DeserializeObject<T>(val);
+        }
+
+        /// <summary>
+        /// 手机号掩码
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string PhoneToAnonymous(this string phone)
+        {
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                throw new ArgumentNullException("phone is empty");
+            }
+
+            return Regex.Replace(phone, "(\\d{3})\\d{4}(\\d{4})", "$1****$2");
         }
     }
 }
